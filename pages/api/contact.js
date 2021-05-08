b906bc9a614be2f33f938fee33a881e0d1240c23
kantiman <krantitherevolution99@gmail.com>
2021-05-08 12:28:19 +0545

    contact-final

--- pages/api/contact.js
+++
@@ -1,7 +0,0 @@
-console.log('before');
-
-export default(req, res) => {
-    console.log('asd');
-    console.log(req.body);
-    res.status(200).json(req.body)
-}
--- pages/contact.js
+++ pages/contact.js
@@ -3,45 +3,40 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 
-import {FaFacebookF, FaTwitter, FaYoutube} from "react-icons/fa"
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaYoutube,
+    FaEnvelope,
+    FaPhone,
+    FaLocationArrow
+} from "react-icons/fa"
 
-import AOS from 'aos';
+import {RiInstagramFill} from "react-icons/ri";
+
 import 'aos/dist/aos.css';
-import {useState} from 'react'
+import emailjs from 'emailjs-com';
 
 
 export default function contact() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [submitted, setSubmitted] = useState(false)
-    const handleSubmit = (e) => {
 
-        e.preventDefault()
-        console.log('Sending')
-        let data = {
-            name,
-            email,
-            message
-        }
-        fetch('/api/contactApi', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then((res) => {
-            console.log('Response received')
-            if (res.status === 200) {
-                console.log('Response succeeded!')
-                setSubmitted(true)
-                setName('')
-                setEmail('')
-                setMessage('')
-            }
-        })
+
+    function onSubmitForm(values) {
+        values.preventDefault();
+
+        emailjs.sendForm('service_xcjnfz9', 'template_2ym61o2', values.target, 'user_5YvxbkhSC6ysNKWIY2G3n').then((result) => {
+            console.log(result.text);
+            const currentDiv = document.getElementById("formMsg");
+            currentDiv.innerHTML = "<p style='color:green'>Congraz ! Your Message has been sent Successfully</p>";
+
+        }, (error) => {
+            console.log(error.text);
+            const currentDiv = document.getElementById("formMsg");
+            currentDiv.innerHTML = "<p style='color:red'>Sorry! Your message was not sent. please try again later</p>";
+        });
+        values.target.reset();
     }
+
     return (
         <>
             <section class="heroBanner aboutHeroBanner"
@@ -78,94 +73,107 @@ export default function contact() {
 
 
             <section class="airBunker">
-
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6 col-sm-6 col-12">
-                            <div className={
-                                styles.container
-                            }>
-                                <form className={
-                                        styles.main
-                                    }
-                                    name="contactForm"
-                                    method="POST">
-                                    <input type="hidden" name="form-name" valus="contactForm"/>
+                        <div class="col-md-12 col-sm-12 col-12">
 
-                                    <formGroup className={
-                                        styles.inputGroup
-                                    }>
-                                        <label htmlFor='name'>Name</label>
-                                        <input type='text'
-                                            onChange={
-                                                (e) => {
-                                                    setName(e.target.value)
-                                                }
-                                            }
-                                            name='name'
-                                            class="inputField form-control"/>
+                            <form class="contactForm" name="contactForm"
+                                onSubmit={onSubmitForm}>
 
-                                    </formGroup>
-                                <formGroup className={
-                                    styles.inputGroup
-                                }>
-                                    <label htmlFor='email'>Email</label>
-                                    <input type='email' name='email'
-                                        onChange={
-                                            (e) => {
-                                                setEmail(e.target.value)
-                                            }
-                                        }
-                                        class="inputField form-control"/>
-                                </formGroup>
-                            <formGroup className={
-                                styles.inputGroup
-                            }>
-                                <label htmlFor='message'>Message</label>
-                                <input type='text' name='message'
-                                    onChange={
-                                        (e) => {
-                                            setMessage(e.target.value)
-                                        }
-                                    }
-                                    class="inputField form-control"/>
-                            </formGroup>
-                        <input type='submit'
-                            onClick={
-                                (e) => {
-                                    handleSubmit(e)
-                                }
-                            }/>
-                    </form>
-            </div>
-        </div>
-    </div>
-</div></section><section class="preFooter"
-    style={
-        {backgroundImage: "url('img/sky.jpeg')"}
-}>
-<div class="shape shape-top" data-negative="true">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-        <path class="shape-fill" d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"></path>
-    </svg>
-</div>
-<div class="container">
-    <h2 class="sectionHeading">Share your flights</h2>
-    <p class="secDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
-        <br/>luctus nec ullamcorper mattis, pulvinar dapibus leo. Praesent feugiat eleifend nisi quis scelerisque.</p>
-    <ul class="socialIcons">
-        <li>
-            <a href="#" target="_blank"><FaFacebookF/></a>
-        </li>
-        <li>
-            <a href="#" target="_blank"><FaTwitter/></a>
-        </li>
-        <li>
-            <a href="#" target="_blank"><FaYoutube/></a>
-        </li>
 
-    </ul>
-</div>
-<div class="background-overlay"></div></section></>
+                                <div class="formGroup">
+                                    <label>Full Name</label>
+                                    <input required name="fullName" class="inputField form-control"/>
+                                </div>
+
+                                <div class="formGroup">
+                                    <label>
+                                        Your Email
+                                    </label>
+                                    <input required type='email' name="email" class="inputField form-control"/>
+                                </div>
+
+                                <div class="formGroup">
+                                    <label>Your Message</label>
+                                    <textarea name="message" class="inputField form-control" required></textarea>
+                                </div>
+                                <input type='submit' class="btn btn-primary btnMain"/>
+                            </form>
+                            <div id="formMsg"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="contactInfo">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="icon"><FaEnvelope/></div>
+                            <h3>
+                                Have Any Questions? ​
+                            </h3>
+                            <p>hi@abc.com<br/>
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="icon"><FaPhone/></div>
+                            <h3>
+                                Call us
+                            </h3>
+
+                            <p>
+                                +01 409 320 6305<br/>
+                                Sales Department
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="icon"><FaLocationArrow/></div>
+                            <h3>Location</h3>
+                            <p>NO.8 West Ru Rong Rd,
+                                <br/>City, Country​</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="preFooter"
+                style={
+                    {backgroundImage: "url('img/sky.jpeg')"}
+            }>
+                <div class="shape shape-top" data-negative="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+                        <path class="shape-fill" d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"></path>
+                    </svg>
+                </div>
+                <div class="container">
+                    <h2 class="sectionHeading">
+                        Follow Us In Social Media
+                    </h2>
+                    <p class="secDescription">We are ready to connect with you!</p>
+                    <ul class="socialIcons">
+                        <li>
+                            <a href="#" target="_blank"><FaFacebookF/></a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank"><FaTwitter/></a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank"><FaYoutube/></a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank"><RiInstagramFill/></a>
+                        </li>
+
+
+                    </ul>
+                </div>
+
+
+                <div class="background-overlay"></div>
+            </section>
+        </>
     )
 }
