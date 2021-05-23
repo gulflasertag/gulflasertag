92b3a4d5f08e50c605c44add26388b09e1d5b537
kranti99 <krantitherevolution99@gmail.com>
2021-05-23 11:07:58 +0545

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -61,7 +61,7 @@ export default function about() {
                             </p>
                         </div>
                         <div class="col-md-6 col-sm-6 col-12">
-                            <img src="img/airbunker2.png" alt="Airbunker"/>
+                            <img src="img/about1.jpg" alt=""/>
                         </div>
                     </div>
                 </div>
@@ -76,7 +76,7 @@ export default function about() {
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6 col-sm-6 col-12">
-                            <img src="img/airbunker.jpeg" alt="Airbunker"/>
+                            <img src="img/about2.jpg" alt=""/>
                         </div>
                         <div class="col-md-6 col-sm-6 col-12">
                             <p class="subheading">
