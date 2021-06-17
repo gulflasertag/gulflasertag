6ea0cf61a1adf117736c1ec5ca69aff54a756171
kranti99 <krantitherevolution99@gmail.com>
2021-06-17 16:34:53 +0545

    Update mars.js

--- pages/mars.js
+++ pages/mars.js
@@ -271,7 +271,7 @@ export default function contact() {
                 <div class="row">
                     <div class="col-md-8 col-sm-8 col-12"></div>
                     <div class="col-md-4 col-sm-4 col-12">
-                        <h2 class="mars_section_title">سوار الصدماتســكــوربــيــون</h2>
+                        <h2 class="mars_section_title">سوار الصدمات <br/>ســكــوربــيــون</h2>
                   
                         <br/>
                         <p class="mars_p">
